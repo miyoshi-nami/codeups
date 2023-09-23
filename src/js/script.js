998965dab03129f11abf70cce10d817263615647
miyoshi-nami <spica_ship1212@yahoo.co.jp>
2023-09-24 08:55:51 +0900

    aboutまで0924

--- src/js/script.js
+++ src/js/script.js
@@ -12,36 +12,51 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
   });
 
-  //slickのスライダー
-    $(".slider")
-      // 最初のスライドに"add-animation"のclassを付ける(data-slick-index="0"が最初のスライドを指す)
-      .on("init", function () {
-        $('.slick-slide[data-slick-index="0"]').addClass("add-animation");
-      })
-      // 通常のオプション
-      .slick({
-        autoplay: true, // 自動再生ON
-        fade: true, // フェードON
-        arrows: false, // 矢印OFF
-        speed: 2000, // スライド、フェードアニメーションの速度1000ミリ秒
-        autoplaySpeed: 4000, // 自動再生速度4000ミリ秒
-        pauseOnFocus: false, // フォーカスで一時停止OFF
-        pauseOnHover: false, // マウスホバーで一時停止OFF
-      })
-      .on({
-        // スライドが移動する前に発生するイベント
-        beforeChange: function (event, slick, currentSlide, nextSlide) {
-          // 表示されているスライドに"add-animation"のclassをつける
-          $(".slick-slide", this).eq(nextSlide).addClass("add-animation");
-          // あとで"add-animation"のclassを消すための"remove-animation"classを付ける
-          $(".slick-slide", this).eq(currentSlide).addClass("remove-animation");
-        },
-        // スライドが移動した後に発生するイベント
-        afterChange: function () {
-          // 表示していないスライドはアニメーションのclassを外す
-          $(".remove-animation", this).removeClass(
-            "remove-animation add-animation"
-          );
-        },
-      });
+
+
+  //slick1のスライダー
+  $(".slider1")
+    // 最初のスライドに"add-animation"のclassを付ける(data-slick-index="0"が最初のスライドを指す)
+    .on("init", function () {
+      $('.slick-slide[data-slick-index="0"]').addClass("add-animation");
+    })
+    // 通常のオプション
+    .slick({
+      autoplay: true, // 自動再生ON
+      fade: true, // フェードON
+      arrows: false, // 矢印OFF
+      speed: 2000, // スライド、フェードアニメーションの速度1000ミリ秒
+      autoplaySpeed: 4000, // 自動再生速度4000ミリ秒
+      pauseOnFocus: false, // フォーカスで一時停止OFF
+      pauseOnHover: false, // マウスホバーで一時停止OFF
+    })
+    .on({
+      // スライドが移動する前に発生するイベント
+      beforeChange: function (event, slick, currentSlide, nextSlide) {
+        // 表示されているスライドに"add-animation"のclassをつける
+        $(".slick-slide", this).eq(nextSlide).addClass("add-animation");
+        // あとで"add-animation"のclassを消すための"remove-animation"classを付ける
+        $(".slick-slide", this).eq(currentSlide).addClass("remove-animation");
+      },
+      // スライドが移動した後に発生するイベント
+      afterChange: function () {
+        // 表示していないスライドはアニメーションのclassを外す
+        $(".remove-animation", this).removeClass(
+          "remove-animation add-animation"
+        );
+      },
+    });
+
+
+
+  //slick2のスライダー
+  $(".slider2").slick({
+    autoplay: false,
+    dots: false,
+    swipe: true,
+    variableWidth:true,
+    prevArrow:false,
+    nextArrow:false,
+    infinite: false,// ループ再生を無効にする
+  });
 });
