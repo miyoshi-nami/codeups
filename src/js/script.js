2d670949b63b9c18ddfad7c02701f59a90b21ebb
miyoshi-nami <spica_ship1212@yahoo.co.jp>
2023-11-18 18:00:21 +0900

    11181800修正

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,13 @@ jQuery(function ($) {
     if ($('.js-hamburger').hasClass('is-open')) {
       $('.js-drawer-menu').fadeOut();
       $(this).removeClass('is-open');
+      $('body').removeClass('is-open');
+      $('gotop').removeClass('is-open');
     } else {
       $('.js-drawer-menu').fadeIn();
       $(this).addClass('is-open');
+      $('body').addClass('is-open');
+      $('gotop').addClass('is-open');
     }
   });
 
